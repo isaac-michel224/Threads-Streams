c801fb7cc8273b8a8daa746a1e5c14473f9d2707
isaac-michel224 <isaac.michel224@gmail.com>
2021-09-29 16:08:52 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+
+    //below is our entry point
+    //it is how we start our application
+    //your main method is in face also the main thread
+    //the main thread as the ability to create additional threads
+
+
+public static void main(String[] args) {
+
+    // below we are instantiating a new thread
+    // and passing in a new instance of HelloRunnable
+    // HelloRunnable is of type Runnable which is one of the
+    // objects that your thread will need to execute
+    new Thread(new HelloRunnable()).start();
+
+    //below we are instantiating a subclass of Thread
+    //that is overriding its own run method
+    new HelloThread().start();
+
+    //bear in mind that Runnable is a functional interface
+    //which means we can use it as a lambda expression
+    //while at first this may seem a little hard to parse
+    //it is arguably clearer, neater and more readable
+
+    new Thread(() -> System.out.println("Hello from a Lambda runnable! ")).start();
+
+    SynchronizedRGB color = new SynchronizedRGB(0,0,0, "Pitch Black");
+
+
+    //this is our first statement
+    int myColorInt = color.getRGB();
+
+    //new Thread accesses color as a resource and invokes "set"
+    new Thread(() -> color.set(255,255,255,"white"));
+
+    //this is our second statement
+
+    String myColorName = color.getName();
+
+    System.out.println(color.getName());
+}
+
+}
